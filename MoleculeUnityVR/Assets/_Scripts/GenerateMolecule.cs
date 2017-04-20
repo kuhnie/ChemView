d69d1ae9f6292559390685050c6e9e380a95e1da
Thomas Bolden <boldenth@gmail.com>
2017-04-20 18:13:31 -0400

    Moving Generate() to separate class

--- MoleculeUnityVR/Assets/_Scripts/GenerateMolecule.cs
+++ MoleculeUnityVR/Assets/_Scripts/GenerateMolecule.cs
@@ -4,9 +4,9 @@
 /// @brief   Implimenting GenerateMolecule class
 ///
 /// This class should be able to generate a 3D GameObject from a data file
-/// .
+/// that is read by the Load[filetype]Data class
 
-/*
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,55 +17,96 @@ using System.Linq;
 
 //---------------------------------------------------------------------------\\
 
-public class LoadCmlData : MonoBehaviour {
+public class GenerateMolecule : MonoBehaviour {
 
-    // public class LoadCmlData ??
-    // OR
-    // public gameobject(?) MoleculeSpawner
+    // in order to make this more universal, could potentially
+    // take last argument as filename and using try() to read based
+    // on the extension
+    // eg. Read(string filename, string filetype)
+    public void Generate(Dictionary<string,Vector3> APD,
+                         Dictionary<string,string> ATD,
+                         List<Dictionary<List<string>,string>> BA,
+                         string NAME){
 
-    //public GameObject HUD; // to display info about hovered over molecule
-    public TextAsset  CmlFile; // file to read from
+        GameObject molecule = new GameObject();
 
-    static string ElementText; // eventually to be used for popup dialog
+        molecule.transform.localScale = new Vector3(1f,1f,1f);
 
-    // atomArray is a list of atoms in the molecule mapping the abbreviated
-    // ptable name to its position
-    // the order of atomArray is same as order in cml file, so index in the
-    // list will reference the atom (eg. atom a2 is second in list)
-    // { "Ag" : (1.0, 2.0, 1.0) }
-    List<Dictionary<string,Vector3>> atomArray
-        = new List<Dictionary<string,Vector3>>(); // or <Element, Vector3> ?
-    
-    // bondArray is a list of all the bonds where each entry looks like
-    // { "bond order" : "atom1 atom2" }
-    List<Dictionary<string,string>> bondArray 
-        = new List<Dictionary<string,string>>();
+        molecule.name = NAME;
 
-    // MOVE THIS LATER !!
-    public void Generate(){
+        //molecule.AddComponent<BoxCollider>();
 
-        // tempDictA - atoms { string : Vector3 }
-        // tempDictB - bonds { string : string }
+        // TODO: fix this!
+        molecule.AddComponent<Rotator>();
 
-        for(int i = 0; i < atomArray.Count; i++){
+        // generate atoms in unity
+        for(int i = 0; i < APD.Count; i++){
 
+            // TODO: make this better for different keys,
+            //   OR: make other files use this format as key
+            string key = "a" + (i+1).ToString();
+
+            // TODO: replace with spawning a prefab
+            // TODO: add support for non-single bonds
             GameObject current = 
                 GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
-            // set position of the atom    
-            current.transform.position = atomArray[i].First().Value;
+            // TODO: add hoverablility support
+            // current.AddComponent<BoxCollider>(); potentially?
 
-            // add more to change shape and color, etc.
+            // set GameObject to be child of the larger molecule
+            current.transform.parent = molecule.transform;
+
+            // set position of the atom    
+            current.transform.position = APD[key];
+
+            // TODO: make this better!
+            // HINT: create an element class with radius and color attributes 
+            //       and use it to set current.transform{scale,material.color}
+            if(ATD[key] == "C"){
+                current.GetComponent<Renderer>().material.color = Color.black;
+                current.transform.localScale = new Vector3(.8f,.8f,.8f);
+            }
+            
+            else if(ATD[key] == "O"){
+                current.GetComponent<Renderer>().material.color = Color.cyan;
+                current.transform.localScale = new Vector3(1f,1f,1f);
+            }
+
+            else if(ATD[key] == "H"){
+                current.GetComponent<Renderer>().material.color = Color.white;
+                current.transform.localScale = new Vector3(.5f,.5f,.5f);
+            }
 
         }
 
-    }
+        // generate bonds
+        for(int j = 0; j < BA.Count; j++){
 
-    void Start() {
+            string atom1 = BA[j].First().Key[0];
+            string atom2 = BA[j].First().Key[1];
+            
+            float length = (APD[atom1] - APD[atom2]).magnitude;
 
-        Read();
-        Generate();
-    
+            GameObject current = 
+                GameObject.CreatePrimitive(PrimitiveType.Capsule);
+
+            // again, set bond as child of molecule
+            current.transform.parent = molecule.transform;
+
+            // setting bond position to halfway between the atoms
+            // using Lerp (linear interpolation)
+            current.transform.position = Vector3.Lerp(APD[atom1],
+                                                      APD[atom2],
+                                                      0.5f);
+            current.transform.localScale = new Vector3(.1f,.1f, length);
+
+            // angling bonds
+            current.transform.LookAt(APD[atom2]);
+            current.GetComponent<Renderer>().material.color = Color.gray;
+
+        }
+
     }
     
     void Update() {
@@ -76,4 +117,3 @@ public class LoadCmlData : MonoBehaviour {
     }
 
 }
-*/
--- MoleculeUnityVR/Assets/_Scripts/LoadCmlData.cs
+++ MoleculeUnityVR/Assets/_Scripts/LoadCmlData.cs
@@ -21,6 +21,8 @@ public class LoadCmlData : MonoBehaviour {
     // TODO: add support to read properties -> assign to molecule parent obj
 
     //public GameObject HUD; // TODO: display info about hovered over molecule
+
+    // TODO: currently needs to be plain text file, can I make it read .cml ?
     public TextAsset  CmlFile; // file to read from
 
     static string ElementText; // eventually to be used for popup dialog
@@ -31,18 +33,18 @@ public class LoadCmlData : MonoBehaviour {
     // atomPosDict is a dict of all atoms in the molecule
     // the keys are the atom id, values are Vector3 position
     // eg. { "a1" : (1.1, 1.2, 1.3) }
-    Dictionary<string,Vector3> atomPosDict 
+    public Dictionary<string,Vector3> atomPosDict 
         = new Dictionary<string,Vector3>();
 
     // atomTypeDict is a dict of all atoms in the molecule
     // keys are same id as above, but value is now the element
     // eg. { "a2" : "Ag" } if the second element was gold
-    Dictionary<string,string> atomTypeDict 
+    public Dictionary<string,string> atomTypeDict 
         = new Dictionary<string,string>();
     
     // bondArray is a list of all the bonds where each entry looks like
     // { ["atom_id1","atom_id2"] : "bond_order" }
-    List<Dictionary<List<string>,string>> bondArray 
+    public List<Dictionary<List<string>,string>> bondArray 
         = new List<Dictionary<List<string>,string>>();
 
     // temporary containers for bonds
@@ -64,9 +66,9 @@ public class LoadCmlData : MonoBehaviour {
                              atom.Attributes["elementType"].Value);
 
             atomPosDict.Add(atom.Attributes["id"].Value,
-                            new Vector3(float.Parse(atom.Attributes["x3"].Value),
-                                        float.Parse(atom.Attributes["y3"].Value),
-                                        float.Parse(atom.Attributes["z3"].Value)));
+                        new Vector3(float.Parse(atom.Attributes["x3"].Value),
+                                    float.Parse(atom.Attributes["y3"].Value),
+                                    float.Parse(atom.Attributes["z3"].Value)));
     
         }
 
@@ -90,99 +92,19 @@ public class LoadCmlData : MonoBehaviour {
 
     }
 
-    // TODO: move this to separate class so generation 
-    //       works with other data files
-    public void Generate(){
-
-        GameObject molecule = new GameObject();
-
-        molecule.transform.localScale = new Vector3(1f,1f,1f);
-
-        molecule.name = moleculeName;
-
-        //molecule.AddComponent<BoxCollider>();
-
-        // TODO: fix this!
-        molecule.AddComponent<Rotator>();
-
-        // generate atoms in unity
-        for(int i = 0; i < atomPosDict.Count; i++){
-
-            // TODO: make this better for different keys,
-            //   OR: make other files use this format as key
-            string key = "a" + (i+1).ToString();
-
-            // TODO: replace with spawning a prefab
-            // TODO: add support for non-single bonds
-            GameObject current = 
-                GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
-            // TODO: add hoverablility support
-            // current.AddComponent<BoxCollider>(); potentially?
-
-            // set GameObject to be child of the larger molecule
-            current.transform.parent = molecule.transform;
-
-            // set position of the atom    
-            current.transform.position = atomPosDict[key];
-
-            // TODO: make this better!
-            // HINT: create an element class with radius and color attributes 
-            //       and use it to set current.transform{scale,material.color}
-            if(atomTypeDict[key] == "C"){
-                current.GetComponent<Renderer>().material.color = Color.black;
-                current.transform.localScale = new Vector3(.8f,.8f,.8f);
-            }
-            
-            else if(atomTypeDict[key] == "O"){
-                current.GetComponent<Renderer>().material.color = Color.cyan;
-                current.transform.localScale = new Vector3(1f,1f,1f);
-            }
-
-            else if(atomTypeDict[key] == "H"){
-                current.GetComponent<Renderer>().material.color = Color.white;
-                current.transform.localScale = new Vector3(.5f,.5f,.5f);
-            }
-
-        }
-
-        // generate bonds
-        for(int j = 0; j < bondArray.Count; j++){
-
-            string atom1 = bondArray[j].First().Key[0];
-            string atom2 = bondArray[j].First().Key[1];
-            
-            float length = (atomPosDict[atom1] - atomPosDict[atom2]).magnitude;
-
-            GameObject current = 
-                GameObject.CreatePrimitive(PrimitiveType.Capsule);
-
-            // again, set bond as child of molecule
-            current.transform.parent = molecule.transform;
-
-            // setting bond position to halfway between the atoms
-            // using Lerp (linear interpolation)
-            current.transform.position = Vector3.Lerp(atomPosDict[atom1],
-                                                      atomPosDict[atom2],
-                                                      0.5f);
-            current.transform.localScale = new Vector3(.1f,.1f, length);
-
-            // angling bonds
-            current.transform.LookAt(atomPosDict[atom2]);
-            current.GetComponent<Renderer>().material.color = Color.gray;
-
-        }
-
-    }
-
-    void Start() {
+    // currently only suppports using one kind of file extension
+    // and needs to be manually dragged onto the MoleculeSpawner
+    // TODO: automatically do this based on a screen selection on
+    //       game start (one option, open to suggestions)
+    void Start(){
 
         Read();
-        Generate();
-    
+        GetComponent<GenerateMolecule>().
+        Generate(atomPosDict, atomTypeDict, bondArray, moleculeName);
+
     }
     
-    void Update() {
+    void Update(){
 
         if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
