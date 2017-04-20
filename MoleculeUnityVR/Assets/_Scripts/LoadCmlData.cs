35e05f1ebc9b29e2bbef1fb5723acef9c846a034
Thomas Bolden <boldenth@gmail.com>
2017-04-20 00:17:50 -0400

    Changing data structures, adding rotation

--- MoleculeUnityVR/Assets/_Scripts/LoadCmlData.cs
+++ MoleculeUnityVR/Assets/_Scripts/LoadCmlData.cs
@@ -23,16 +23,18 @@ public class LoadCmlData : MonoBehaviour {
 
     static string ElementText; // eventually to be used for popup dialog
 
+    public string moleculeName = "Molecule"; // TODO: change to customizable!
+
     // atomArray is a list of atoms in the molecule mapping the abbreviated
     // ptable name to its position
     // the order of atomArray is same as order in cml file, so index in the
     // list will reference the atom (eg. atom a2 is second in list)
     // { "Ag" : (1.0, 2.0, 1.0) }
 
-    // atomPosAry is a list of positions of each atom in the molecule
+    // atomPosDict is a list of positions of each atom in the molecule
     // mapping the position of the atom based 
-    List<Vector3> atomPosAry 
-        = new List<Vector3>();
+    Dictionary<string,Vector3> atomPosDict 
+        = new Dictionary<string,Vector3>();
 
     List<Dictionary<string,string>> atomTypeDict 
         = new List<Dictionary<string,string>>();
@@ -43,7 +45,8 @@ public class LoadCmlData : MonoBehaviour {
         = new List<Dictionary<List<string>,string>>();
 
     // temporary dictionaries for atoms and bonds, respectively
-    Dictionary<string,string> tempDictA;
+    Dictionary<string,string> tempBondDictA;
+    //Dictionary<string,Vector3> tempPosDictA;
 
     Dictionary<List<string>,string> tempDictB;
     List<string> tempListB;
@@ -60,15 +63,19 @@ public class LoadCmlData : MonoBehaviour {
         // atoms should now be everything tagged "atom"
         foreach(XmlNode atom in atoms){
 
-            tempDictA = new Dictionary<string,string>();
-            tempDictA.Add(atom.Attributes["id"].Value,
-                          atom.Attributes["elementType"].Value);
+            tempBondDictA = new Dictionary<string,string>();
+            tempBondDictA.Add(atom.Attributes["id"].Value,
+                              atom.Attributes["elementType"].Value);
 
-            atomPosAry.Add(new Vector3(float.Parse(atom.Attributes["x3"].Value),
-                                       float.Parse(atom.Attributes["y3"].Value),
-                                       float.Parse(atom.Attributes["z3"].Value)));
+            //tempPosDictA = new Dictionary<string,Vector3>();
 
-            atomTypeDict.Add(tempDictA);
+            atomPosDict.Add(atom.Attributes["id"].Value,
+                              new Vector3(float.Parse(atom.Attributes["x3"].Value),
+                                          float.Parse(atom.Attributes["y3"].Value),
+                                          float.Parse(atom.Attributes["z3"].Value)));
+
+            atomTypeDict.Add(tempBondDictA);
+            //atomPosDict.Add(tempPosDictA);
     
         }
 
@@ -98,17 +105,29 @@ public class LoadCmlData : MonoBehaviour {
         // tempDictA - atoms { string : Vector3 }
         // tempDictB - bonds { string : string }
 
+        GameObject molecule = new GameObject();
+            //GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        molecule.transform.localScale = new Vector3(1f,1f,1f);
+
+        molecule.name = moleculeName;
+
+        // TODO: fix this!
+        //molecule.AddComponent<Rotator>();
+
         // generate atoms
-        for(int i = 0; i < atomPosAry.Count; i++){
+        for(int i = 0; i < atomPosDict.Count; i++){
+
+            string key = "a" + (i+1).ToString();
 
             // TODO: replace with spawning a prefab
-            // TODO: make these children of an empty 
-            //       so it can all rotate together!
             GameObject current = 
                 GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
+            current.transform.parent = molecule.transform;
+
             // set position of the atom    
-            current.transform.position = atomPosAry[i];
+            current.transform.position = atomPosDict[key];
 
             if(atomTypeDict[i].First().Value == "C")
                 current.GetComponent<Renderer>().material.color = Color.black;
@@ -130,12 +149,20 @@ public class LoadCmlData : MonoBehaviour {
             //
             string atom1 = bondArray[j].First().Key[0];
             string atom2 = bondArray[j].First().Key[1];
-
-            //Vector3 atom1loc =
-
-            //Debug.Log(atom1);
             
-            //float length = (atomArray)
+            float length = (atomPosDict[atom1] - atomPosDict[atom2]).magnitude;
+
+            GameObject current = 
+                GameObject.CreatePrimitive(PrimitiveType.Capsule);
+
+            current.transform.parent = molecule.transform;
+
+            current.transform.position = Vector3.Lerp(atomPosDict[atom1],
+                                                      atomPosDict[atom2],
+                                                      0.5f);
+            current.transform.localScale = new Vector3(.1f,.1f, length);
+            current.transform.LookAt(atomPosDict[atom2]);
+            current.GetComponent<Renderer>().material.color = Color.gray;
 
         }
 
@@ -149,6 +176,13 @@ public class LoadCmlData : MonoBehaviour {
     }
     
     void Update() {
+
+        // rotating the molecule
+        // in VR, this should somehow work with the VR sticks/controllers
+
+        //GameObject molecule = GameObject.Find(moleculeName);
+
+        //molecule.transform.Rotate(0,20*Time.deltaTime,0);
 
         if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
---
+++ MoleculeUnityVR/Assets/_Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class Rotator : MonoBehaviour {
+
+    //public LoadCmlData cmlDataLoader;
+     
+    //string name = GetComponent<LoadCmlData>().MoleculeName;
+
+    void Update(){
+
+        //GameObject molecule = GameObject.Find(name);
+
+        //molecule.transform.Rotate(0,20*Time.deltaTime,0);
+
+    }
+     
+}
